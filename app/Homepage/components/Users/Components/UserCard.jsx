b6deeabaa488003eb7cb6d5a494dca6ff3cc5aa2
Axel <johnaxelcortez21@gmail.com>
2024-08-16 14:51:04 +1000

    Change index to key

--- app/Homepage/components/Users/Components/UserCard.jsx
+++ app/Homepage/components/Users/Components/UserCard.jsx
@@ -11,7 +11,7 @@ import {
   Input,
 } from "@nextui-org/react";
 
-const UserCard = ({ user, index, checkStat, tabLoc, tabSelect }) => {
+const UserCard = ({ user, key, checkStat, tabLoc, tabSelect }) => {
   console.log(tabSelect);
   const locationData = ["Laptop", "Monitor", "Peripheral"];
   const summaryData = (loc) => {
@@ -52,7 +52,7 @@ const UserCard = ({ user, index, checkStat, tabLoc, tabSelect }) => {
   };
   return (
     <Card
-      key={index}
+      key={key}
       className={`
     ${checkVisibility()} hover:cursor-pointer`}
     >
